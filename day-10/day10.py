58c88daca57f612bc8c5a29f29dd2e8eaa06d707
Romy <romy.bos@hotmail.com>
2021-12-11 15:55:59 +0000

    completed day 10 part 2

--- day-10/day10.py
+++ day-10/day10.py
@@ -5,13 +5,16 @@ OPENING_CHARS = ['(', '[', '{', '<']
 CLOSING_CHARS = [')', ']', '}', '>']
 CHARSDICT = {'(': ')', '[': ']', '{': '}', '<': '>'}
 CHARSSCOREDICT = {')': 3, ']': 57, '}': 1197, '>': 25137}
+CHARSSCOREDICTPART2 = {')': 1, ']': 2, '}': 3, '>': 4}
 
 def getIllegalCharacters(inputfile):
     illegalCharsList = []
+    closingCharsList = []
     lines = inputfile.readlines()
-    for i in range(len(lines)):
+    for i in range(len(lines) -1, -1, -1):
         openCharStack = []
         illegalCharsListLine = []
+        closingCharsListLine = []
         expectedClosingChar = ''
         line = list(lines[i].strip())
         for char in line:
@@ -29,7 +32,12 @@ def getIllegalCharacters(inputfile):
                     expectedClosingChar = CHARSDICT[openCharStack[-1]]
         if illegalCharsListLine:
             illegalCharsList.append(illegalCharsListLine[0])
-    return illegalCharsList
+        else:
+            for i in range(len(openCharStack) -1, -1, -1):
+                char = openCharStack[i]
+                closingCharsListLine.append(CHARSDICT[char])
+            closingCharsList.append(closingCharsListLine)
+    return illegalCharsList, closingCharsList[::-1]
 
 def getSyntaxScore(illegalCharsList):
     syntaxScore = 0
@@ -37,7 +45,22 @@ def getSyntaxScore(illegalCharsList):
         syntaxScore += CHARSSCOREDICT[char]
     return syntaxScore
 
-illegalCharsList = getIllegalCharacters(inputfile)
+def getAutoCompleteScore(closingCharsList):
+    autoCompleteScore = 0
+    autoCompleteScoreList = []
+    for charList in closingCharsList:
+        lineScore = 0
+        for char in charList:
+            lineScore = lineScore * 5
+            lineScore += (CHARSSCOREDICTPART2[char])
+        autoCompleteScoreList.append(lineScore)
+    autoCompleteScoreList.sort()
+    return autoCompleteScoreList[len(autoCompleteScoreList)//2]
+
+illegalCharsList, closingCharsList = getIllegalCharacters(inputfile)
 print(illegalCharsList)
 syntaxScore = getSyntaxScore(illegalCharsList)
 print(syntaxScore)
+print()
+print(closingCharsList)
+print(getAutoCompleteScore(closingCharsList))
